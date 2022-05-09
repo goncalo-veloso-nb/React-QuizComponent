209006e0dc836f7c691263f87175f9de13ebde6d
NOVABASE\NB28354 <nbNB28354@novabase.pt>
2022-05-09 15:04:21 +0100

    Fix: add props to quiz constructor

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = { quiz_position: 1 };
     }
 
